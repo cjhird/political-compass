1cbc773db7c4a02ef30d579deebba912fc5e2300
cjhird <charlie.hird@googlemail.com>
2022-11-19 23:36:58 +0000

    pseudocode

--- src/comp/helpers/Engine.js
+++ src/comp/helpers/Engine.js
@@ -1,1 +1,9 @@
 // test logic here
+
+// spectrum x / x-axis: economic views
+// secptrum y / y-axis: social views
+
+// uses variable to measure user score on two spectrums
+// takes in the input of user responses to statements
+// user input for each statement is rated and added to the user's spectrum scores - increases/decreases
+// user scores for each spectrum are exported to the graph component to be plotted.
