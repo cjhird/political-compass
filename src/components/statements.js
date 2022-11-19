03c21f20e0ede8357958a314ec48dc392065c21b
cjhird <charlie.hird@googlemail.com>
2022-11-19 22:18:46 +0000

    some questions added and refactoring

---
+++ src/components/statements.js
@@ -0,0 +1,30 @@
+const statements = [
+  {
+    statement: 'The freer the market, the feer the people.',
+    responseVal: true,
+    theme: 'economy',
+  },
+  {
+    statement:
+      'Possessing marijuana for personal use should not be a criminal offence.',
+    responseVal: true,
+    theme: 'social',
+  },
+  {
+    statement: 'Land shouldn’t be a commodity to be bought and sold.',
+    responseVal: true,
+    theme: 'economy',
+  },
+  {
+    statement:
+      'The death penalty should be an option for the most serious crimes.',
+    responseVal: true,
+    theme: 'social',
+  },
+  {
+    statement:
+      'The only social responsibility of a company should be to deliver a profit to its shareholders.',
+    responseVal: true,
+    theme: 'economy',
+  },
+]
