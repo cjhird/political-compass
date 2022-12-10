006f348767b43051365c9f3bd9e5353c8b394b92
cjhird <charlie.hird@googlemail.com>
2022-12-10 04:01:47 +0000

    testview pseudo

--- src/comp/pages/TestView.js
+++ src/comp/pages/TestView.js
@@ -1,4 +1,19 @@
 const TestView = () => {
+  // Sections: A, B, C and D w/ six statements each
+
+  // ! loadStatements function
+  // check the current section.
+  // load the set of 6 statements for specific section
+
+  // ! inputValue function
+  // Take user input on political statements
+  // each statment will have a direction: right or left - this determines value of the input.
+  // e.g. if the statement is left leaning and the user strongly agrees: economicScore += -2.00
+  // e.g. whereas if statement is right leaning and user strongly agrees: economicScore += 2.00
+  // e.g. whereas if statment is left leaning and user strongly disagrees: economicScore += 2.00
+  // e.g. whereas if statement is right leaning and user strongly disagress: economicScore += -2.00
+  // Based on statement direction and the value of the input selected, +/- value from economicScore & socialScore
+
   return (
     <div className="testview-wrapper">
       <h1>TestView</h1>
