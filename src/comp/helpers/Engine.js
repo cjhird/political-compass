b9965d80323a19eefd836f4b8e892433dea70ae4
cjhird <charlie.hird@googlemail.com>
2022-12-10 03:52:10 +0000

    pseudocode

--- src/comp/helpers/Engine.js
+++ src/comp/helpers/Engine.js
@@ -1,9 +1,14 @@
 // test logic here
 
-// spectrum x / x-axis: economic views
-// secptrum y / y-axis: social views
+// 3 Game Counters:
+// Economic Score - Left (Socialism) vs Right (Capitalism)
+// Social Score - Authoritarian vs Libertarian
+// Test Progress - e.g. 34 of 60
 
-// uses variable to measure user score on two spectrums
-// takes in the input of user responses to statements
-// user input for each statement is rated and added to the user's spectrum scores - increases/decreases
-// user scores for each spectrum are exported to the graph component to be plotted on the quadrant
+// ! inputValue function
+// Take user input on political statements
+// each statment will have a direction: right or left - this determines value of the input.
+// e.g. if the statement is left leaning and the user strongly agrees: economicScore += -2.00
+// e.g. whereas if statement is right leaning and user strongly agrees: economicScore += 2.00
+// e.g. whereas if statment is left leaning and user strongly disagrees: economicScore += 2.00
+// e.g. whereas if statement is right leaning and user strongly disagress: economicScore += -2.00
--- src/comp/pages/Landing.js
+++ src/comp/pages/Landing.js
@@ -1,9 +1,18 @@
+import { Link } from 'react-router-dom'
+
 const Landing = () => {
   return (
     <>
       <div className="landing-wrapper">
-        <h1>Landing page</h1>
-        <h2>This is the landing page</h2>
+        <h1>Welcome to the Political Compass!</h1>
+        <h2>
+          This website uses responses to a set of 62 propositions to rate <br />
+          political ideology in a political spectrum with two axes: economic and
+          social{' '}
+        </h2>
+        <Link to="/test">
+          <button>START TEST</button>
+        </Link>
       </div>
     </>
   )
